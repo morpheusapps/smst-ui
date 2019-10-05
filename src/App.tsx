0ecf1a9018513fe0fe764981509379612e355740
maorRoz <andory@post.bgu.ac.il>
2019-10-05 03:43:37 +0300

    added temp login ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,28 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom';
+import { Button } from './Button';
 
-const App = (): JSX.Element => <div>hello</div>;
+const App = (): JSX.Element => (
+  <div style={{ display: 'flex', justifyContent: 'center' }}>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <div style={{ marginTop: '500px' }}>
+            <Link style={{ textDecoration: 'none', color: 'white' }} to="/home">
+              <Button>Sign in with Google</Button>
+            </Link>
+          </div>
+        </Route>
+        <Route exact path="/home">
+          <div style={{ marginTop: '500px' }}>
+            <Link style={{ textDecoration: 'none', color: 'white' }} to="/">
+              <Button>Sign out</Button>
+            </Link>
+          </div>
+        </Route>
+      </Switch>
+    </Router>
+  </div>
+);
 
 export default App;
---
+++ src/Button.tsx
@@ -0,0 +1,15 @@
+import styled from 'styled-components';
+
+export const Button = styled.div`
+  padding: 10px;
+  border-radius: 10px;
+  width: 150px;
+  height: 20px;
+  background-color: #d95327;
+  color: white;
+  text-align: center;
+  &:hover {
+    cursor: pointer;
+    opacity: 0.8;
+  }
+`;
