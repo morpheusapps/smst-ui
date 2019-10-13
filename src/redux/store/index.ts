c0c2404fc4e006b0489bb01eb2325b65083e6d49
maorRoz <andory@post.bgu.ac.il>
2019-10-13 21:14:57 +0300

    added more tests command

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,16 +1,1 @@
-import { createStore, Store, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import { rootReducer, SemesterionStates } from '../reducers';
-import { rootSaga } from '../sagas';
-import { sagaMiddleware } from '../middlewares';
-
-const configureStore = (): Store<SemesterionStates> => {
-  const store = createStore(
-    rootReducer,
-    composeWithDevTools(applyMiddleware(sagaMiddleware))
-  );
-  sagaMiddleware.run(rootSaga);
-  return store;
-};
-
-export const store = configureStore();
+export { store } from './store';
