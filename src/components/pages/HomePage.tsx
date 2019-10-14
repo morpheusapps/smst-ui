82a1be2c4d62008cb48511f5f375a881b1539fdb
maorRoz <andory@post.bgu.ac.il>
2019-10-14 19:54:10 +0300

    added smst-design-system usage

--- src/components/pages/HomePage.tsx
+++ src/components/pages/HomePage.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { Button } from 'smst-design-system';
 import { SemesterionStates, Logout } from '../../redux';
-import { Button } from '../Button';
 
 export const HomePage = () => {
   const email = useSelector(({ auth }: SemesterionStates) => auth.profile);
