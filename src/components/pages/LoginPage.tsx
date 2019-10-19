9e6810128a454b75c22a525ab8e72de3cfa043b1
maorRoz <andory@post.bgu.ac.il>
2019-10-19 03:55:43 +0300

    added better global style

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { Button } from '../Button';
+import { GoogleAuthButton } from 'smst-design-system';
 
 export const LoginPage = () => (
-  <a
-    href={`${process.env.REACT_APP_BACKEND_AUTH}/api/auth/google`}
-    style={{ textDecoration: 'none', color: 'white' }}
-  >
-    <Button>Sign in with Google</Button>
-  </a>
+  <GoogleAuthButton
+    url={`${process.env.REACT_APP_BACKEND_AUTH}/api/auth/google`}
+  />
 );
